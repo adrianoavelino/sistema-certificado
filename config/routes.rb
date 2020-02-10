e8cdb4b42ca5b156ca7c6e690bc1433832f69733
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-10 20:59:37 -0300

    adiciona o controller de usuários

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :usuarios, controllers: { registrations: 'usuarios/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :usuarios
   root 'home#index'
 end
