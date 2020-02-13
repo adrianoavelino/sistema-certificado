fada80e92d01d04ac1e40cb20b4b8079d9903069
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-12 23:06:23 -0300

    configura o tema no rails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import "../stylesheets/application";
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -11,6 +12,17 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+
+document.addEventListener("turbolinks:load", () => {
+  $('#menu_toggle').on('click', function () {
+    if ($('body').hasClass('nav-md')) {
+      $(".logo").show()
+    } else {
+      $(".logo").hide()
+    }
+  });
+
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
