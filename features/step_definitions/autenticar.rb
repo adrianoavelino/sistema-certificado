f2300bcee9bb0cb07c85a450df384bd26e81708e
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-13 17:17:43 -0300

    adicione os steps para a feature autenticação

---
+++ features/step_definitions/autenticar.rb
@@ -0,0 +1,27 @@
+Dado("que eu sou um usuário cadastrado") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado("visito a página de login") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu enviar as informações de login") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("eu devo ter acesso ao sistema") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado("que eu sou um usuário autenticado") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu clico no link de sair") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("eu devo acessar a página de login") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
