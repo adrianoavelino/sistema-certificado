47f986f4dec771483588318ab80f1d69aed56f94
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-14 23:17:07 -0300

    adiciona o teste de controler usuario #create

--- spec/controllers/usuarios_controller_spec.rb
+++ spec/controllers/usuarios_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe UsuariosController, type: :controller do
-
+  describe 'Como um usuário logado' do
+    before (:each) do
+    end
+    context 'POST #create' do
+      it 'cria um novo usuário' do
+        @request.env['devise.mapping'] = Devise.mappings[:user]
+        @current_usuario = FactoryBot.create(:usuario)
+        sign_in @current_usuario
+        usuario = FactoryBot.attributes_for(:usuario)
+        expect {
+          post :create, params: { usuario: { email: usuario[:email], password: usuario[:password] } }
+        }.to change(Usuario, :count).by 1
+      end
+    end
+  end
 end
