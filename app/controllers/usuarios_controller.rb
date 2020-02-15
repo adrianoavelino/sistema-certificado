d09b85086b6aa4f0e05cda09242fa64d689605c5
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-14 23:20:03 -0300

    adiciona create e new para o controller do usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,4 +1,22 @@
 class UsuariosController < ApplicationController
   def index
   end
+
+  def new
+    @usuario = Usuario.new
+  end
+
+  def create
+    @usuario = Usuario.new(usuario_params)
+    if @usuario.save
+      redirect_to new_usuario_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def usuario_params
+    params.require(:usuario).permit(:email, :password)
+  end
 end
