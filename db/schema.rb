79cdf5f598472555e07320bea0adcf3afed965f8
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-21 12:32:00 -0300

    adiciona o model aluno

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_200609) do
+ActiveRecord::Schema.define(version: 2020_02_21_151158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "alunos", force: :cascade do |t|
+    t.string "nome"
+    t.string "ra"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "usuarios", force: :cascade do |t|
     t.string "email", default: "", null: false
