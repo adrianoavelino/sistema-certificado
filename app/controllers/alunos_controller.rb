5dfc375e6006b8105ba143889f42026a8f31a71d
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-21 16:00:29 -0300

    adiciona o controller de aluno para o cenário de cadastro de novo aluno

---
+++ app/controllers/alunos_controller.rb
@@ -0,0 +1,19 @@
+class AlunosController < ApplicationController
+  def new
+    @aluno = Aluno.new
+  end
+
+  def create
+    @aluno = Aluno.new(aluno_params)
+    if @aluno.save
+      redirect_to new_aluno_path, notice: 'Aluno cadastrado com sucesso!'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def aluno_params
+    params.require(:aluno).permit(:nome, :ra)
+  end
+end
