6e8077611684aa464f171245e4594d621111084c
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-21 15:59:00 -0300

    implementa os passos para o cenário de cadastro de novo aluno

--- features/step_definitions/gerenciar_aluno.rb
+++ features/step_definitions/gerenciar_aluno.rb
@@ -1,11 +1,18 @@
 Dado("que eu visito a página de cadastro de alunos") do
-  #
+  visit new_aluno_path
 end
 
 Quando("eu enviar as informações do cadastro do aluno") do
-  #
+  preencher_form_aluno
+  click_button "Criar Aluno"
 end
 
-Então("eu devo ver as informações do novo aluno") do
-  #
+Então("eu devo ver uma notificação de aluno cadastrado") do
+  expect(page).to have_content "Aluno cadastrado com sucesso!"
+end
+
+def preencher_form_aluno
+  aluno = FactoryBot.build(:aluno)
+  fill_in "aluno_nome", with: aluno.nome
+  fill_in "aluno_ra", with: aluno.ra
 end
