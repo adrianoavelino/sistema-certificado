ea97194c2847f1c13fa9fb34c22976697c7b40a5
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-21 15:27:07 -0300

    adiciona as rotas para o aluno

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :usuarios, controllers: { registrations: 'usuarios/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :usuarios
+  resources :alunos, only: [:new, :create, :update, :destroy]
   root 'home#index'
 end
