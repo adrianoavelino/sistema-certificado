4056100bebc0eb58af53176f171665beeacd5e40
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-26 21:35:04 -0300

    refatora os testes para preencher form

--- features/step_definitions/gerenciar_aluno.rb
+++ features/step_definitions/gerenciar_aluno.rb
@@ -3,7 +3,8 @@ Dado("que eu visito a página de cadastro de alunos") do
 end
 
 Quando("eu enviar as informações do cadastro do aluno") do
-  preencher_form_aluno
+  aluno = FactoryBot.build(:aluno)
+  preencher_form_aluno(aluno)
   click_button "Criar Aluno"
 end
 
@@ -13,6 +14,7 @@ end
 
 Quando("eu enviar as informações do cadastro do aluno com ra repetido") do
   aluno = FactoryBot.create(:aluno)
+  preencher_form_aluno(aluno)
   fill_in "aluno_nome", with: aluno.nome
   fill_in "aluno_ra", with: aluno.ra
   click_button "Criar Aluno"
@@ -22,8 +24,7 @@ Então("eu devo ver uma notificação de ra do aluno em uso") do
   expect(page).to have_content "Ra já está em uso"
 end
 
-def preencher_form_aluno
-  aluno = FactoryBot.build(:aluno)
+def preencher_form_aluno(aluno)
   fill_in "aluno_nome", with: aluno.nome
   fill_in "aluno_ra", with: aluno.ra
 end
