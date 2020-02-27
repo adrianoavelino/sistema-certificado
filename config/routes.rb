0832cd888df6d4dcd512633af7d5c0f9da5d81c0
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-26 22:13:50 -0300

    adicoina a página de visualização de alunos

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :usuarios, controllers: { registrations: 'usuarios/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :usuarios
-  resources :alunos, only: [:new, :create, :update, :destroy]
+  resources :alunos
   root 'home#index'
 end
