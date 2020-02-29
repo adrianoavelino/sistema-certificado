6b9a334f2b61b4b2c98750794c921baef9dff124
Adriano Avelino <adriano-st777@hotmail.com>
2020-02-29 14:02:03 -0300

    adiciona a rota de evento

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :usuarios
   resources :alunos
+  resources :eventos
   root 'home#index'
 end
