4737c2fd36bdf84dd08db619b9ceecd09217f8e3
Adriano Avelino <adriano-st777@hotmail.com>
2020-03-14 23:11:28 -0300

    adiciona o campo anexo no model certificado

--- app/models/certificado.rb
+++ app/models/certificado.rb
@@ -3,4 +3,9 @@ class Certificado < ApplicationRecord
   belongs_to :evento
   belongs_to :certification_type
   belongs_to :emission_sector
+
+  has_one_attached :anexo
+  validates :anexo, attached:true,
+    size: { less_than: 1.megabytes },
+    content_type: %i[pdf]
 end
