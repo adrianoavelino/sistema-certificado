f0acf3bb2e39b85e521481516b14546c31dcbe10
Adriano Avelino <adriano-st777@hotmail.com>
2020-03-16 23:24:12 -0300

    adiciona validações para o certificado

--- app/models/certificado.rb
+++ app/models/certificado.rb
@@ -5,6 +5,12 @@ class Certificado < ApplicationRecord
   belongs_to :emission_sector
 
   has_one_attached :anexo
+  validates :ano, presence: true
+  validates :data_emissao, presence: true
+  validates :titulo, presence: true
+  validates :periodo_inicial, presence: true
+  validates :periodo_final, presence: true
+  validates :carga_horaria, presence: true
   validates :anexo, attached:true,
     size: { less_than: 1.megabytes },
     content_type: %i[pdf]
