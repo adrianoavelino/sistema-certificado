177418ae567718b62f2ddce30f634e4318b3bc2e
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-19 17:52:12 -0300

    adiciona o controller para remessas

---
+++ app/controllers/shipping_controller.rb
@@ -0,0 +1,11 @@
+require "./app/services/convert_string_to_array_numbers_service"
+
+class ShippingController < ApplicationController
+  def index
+    @term = params[:term] || ""
+    certification_numbers = ConvertStringToArrayNumbersService.perform(@term)
+    @certificados = Certificado.includes(:aluno)
+                               .where(id: certification_numbers)
+                               .order(:id)
+  end
+end
