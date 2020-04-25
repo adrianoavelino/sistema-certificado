45acff631b32a7a3f82d3caab8240bbada219e7e
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-24 23:33:08 -0300

    remove devise model usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :usuarios, controllers: { registrations: 'usuarios/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :usuarios
   resources :alunos
   resources :eventos
   resources :emission_sectors
--- spec/factories/usuarios.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :usuario do
-    email {FFaker::Internet.email}
-    password {FFaker::Internet.password}
-  end
-end
