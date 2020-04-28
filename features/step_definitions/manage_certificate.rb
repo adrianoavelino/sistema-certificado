a2221c864d465d166743422562a1dfa1dc915d81
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-27 22:04:54 -0300

    atualiza os testes da feature manage certificate

--- features/step_definitions/manage_certificate.rb
+++ features/step_definitions/manage_certificate.rb
@@ -6,18 +6,18 @@ E("tenho um participante, um evento, um certification_type e um emission_sector 
 end
 
 E ("que eu visito a página de cadastro de certificados") do
-  visit new_certificado_path
+  visit new_certificate_path
 end
 
 Quando("eu enviar as informações do cadastro do certificado") do
-  certificado = FactoryBot.build(
-    :certificado,
+  certificate = FactoryBot.build(
+    :certificate,
     participant_id: @participant.id,
     event_id: @event.id,
     certification_type_id: @certification_type.id,
     emission_sector_id: @emission_sector.id
   )
-  preencher_form_certificado(certificado)
+  full_certificate_form(certificate)
   click_button "Criar Certificado"
 end
 
@@ -34,79 +34,78 @@ Então("eu devo ver uma notificação de certificado cadastrado") do
 end
 
 Dado("que eu tenho {int} certificados") do |qtd|
-  certificado = FactoryBot.create_list( :certificado, qtd)
-  expect(Certificado.count).to eq qtd
+  certificate = FactoryBot.create_list( :certificate, qtd)
+  expect(Certificate.count).to eq qtd
 end
 
 Dado("eu acessar a página de certificados") do
-  visit certificados_path
+  visit certificates_path
 end
 
 Então("eu devo visualizar {int} certificados na lista de certificados") do |int|
-  certificado_um = Certificado.first
-  certificado_dois = Certificado.second
-  certificado_tres = Certificado.third
-  certificado_quatro = Certificado.fourth
-  expect(page).to have_content certificado_um.titulo
-  expect(page).to have_content certificado_dois.titulo
-  expect(page).to have_content certificado_tres.titulo
-  expect(page).to have_content certificado_quatro.titulo
+  certificate_um = Certificate.first
+  certificate_dois = Certificate.second
+  certificate_tres = Certificate.third
+  certificate_quatro = Certificate.fourth
+  expect(page).to have_content certificate_um.title
+  expect(page).to have_content certificate_dois.title
+  expect(page).to have_content certificate_tres.title
+  expect(page).to have_content certificate_quatro.title
 end
 
 Dado("que eu tenho {int} certificado cadastrado") do |int|
-  certificado = FactoryBot.create( :certificado)
-  expect(Certificado.count).to eq int
+  certificate = FactoryBot.create( :certificate)
+  expect(Certificate.count).to eq int
 end
 
 Quando("deletar um certificado") do
-  @certificado_deletado = Certificado.last
+  @deleted_certificate = Certificate.last
   all('[data-toggle="modal"]').last.click
   all('a.btn-danger').last.click
 end
 
 Então("eu não devo visualizar as informações desse certificado") do
-  expect(page).not_to have_content @certificado_deletado.titulo
+  expect(page).not_to have_content @deleted_certificate.title
   expect(page).to have_content 'Certificado excluído com sucesso!'
 end
 
 Dado("que eu acesso a página de atualização do certificado") do
-  FactoryBot.create(:certificado)
-  visit certificados_path
-  all('.edit_certificado').last.click
+  FactoryBot.create(:certificate)
+  visit certificates_path
+  all('.edit_certificate').last.click
 end
 
 Quando("eu alterar as informações do certificado") do
-  @certificado_atualizado = FactoryBot.build(
-    :certificado,
+  @updated_certificate = FactoryBot.build(
+    :certificate,
     participant_id: @participant.id,
     event_id: @event.id,
     certification_type_id: @certification_type.id,
     emission_sector_id: @emission_sector.id
   )
-  preencher_form_certificado(@certificado_atualizado)
+  full_certificate_form(@updated_certificate)
   click_button "Atualizar Certificado"
 end
 
 Então("eu devo visualizar as informações atualizadas desse certificado") do
-  expect(page).to have_content @certificado_atualizado.titulo
+  expect(page).to have_content @updated_certificate.title
 end
 
-def preencher_form_certificado(certificado)
-  fill_in "certificado_ano", with: certificado.ano
-  fill_in "certificado_data_emissao", with: certificado.data_emissao
-  fill_in "certificado_data_emissao", with: certificado.data_emissao
-  select certificado.participant.name, :from => "certificado_participant_id"
-  select certificado.event.description, :from => "certificado_event_id"
-  fill_in "certificado_titulo", with: certificado.titulo
-  fill_in "certificado_periodo_inicial", with: certificado.periodo_inicial
-  fill_in "certificado_periodo_final", with: certificado.periodo_final
-  fill_in "certificado_carga_horaria", with: certificado.carga_horaria
-  select certificado.certification_type.description, :from => "certificado_certification_type_id"
-  fill_in "certificado_dados_adicionais", with: certificado.dados_adicionais
-  select certificado.emission_sector.description, :from => "certificado_emission_sector_id"
+def full_certificate_form(certificate)
+  fill_in "certificate_year", with: certificate.year
+  fill_in "certificate_date_issue", with: certificate.date_issue
+  select certificate.participant.name, :from => "certificate_participant_id"
+  select certificate.event.description, :from => "certificate_event_id"
+  fill_in "certificate_title", with: certificate.title
+  fill_in "certificate_initial_period", with: certificate.initial_period
+  fill_in "certificate_final_period", with: certificate.final_period
+  fill_in "certificate_workload", with: certificate.workload
+  select certificate.certification_type.description, :from => "certificate_certification_type_id"
+  fill_in "certificate_additional_data", with: certificate.additional_data
+  select certificate.emission_sector.description, :from => "certificate_emission_sector_id"
   file_path = Rails.root.join("features", "support", "assets", "test.pdf")
-  attach_file("certificado_anexo", file_path)
-  fill_in "certificado_observacoes", with: certificado.observacoes
+  attach_file("certificate_anexo", file_path)
+  fill_in "certificate_observation", with: certificate.observation
 end
 
 After do
