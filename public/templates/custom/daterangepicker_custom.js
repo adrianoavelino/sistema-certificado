36c5d34f7e3befc90789ef84ba738dbb31085454
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-29 21:24:26 -0300

    corrige daterangepicker para preecher data após selecionar calendário

---
+++ public/templates/custom/daterangepicker_custom.js
@@ -0,0 +1,58 @@
+$(function() {
+  $.fn.singleDatePicker = function() {
+    $(this).on("apply.daterangepicker", function(e, picker) {
+      picker.element.val(picker.startDate.format(picker.locale.format));
+    });
+    return $(this).daterangepicker({
+      singleDatePicker: true,
+      autoUpdateInput: false,
+      minDate: getMindate(),
+      maxDate: getMaxdate(),
+      locale: {
+        format: "DD/MM/YYYY",
+        separator: " - ",
+        applyLabel: "Aplicar",
+        cancelLabel: "Cancelar",
+        daysOfWeek: [
+          "Dom",
+          "Seg",
+          "Ter",
+          "Qua",
+          "Qui",
+          "Sex",
+          "Sab"
+        ],
+        monthNames: [
+          "Janeiro",
+          "Fevereiro",
+          "Março",
+          "Abril",
+          "Maio",
+          "Junho",
+          "Julho",
+          "Agosto",
+          "Setembro",
+          "Outubro",
+          "Novembro",
+          "Dezembro"
+        ],
+        firstDay: 1
+      }
+    });
+  };
+
+  function getMaxdate() {
+    let today = new Date();
+    return new Date(today.setMonth(today.getMonth() + 1))
+  }
+
+  function getMindate() {
+    let today = new Date();
+    return new Date(today.setMonth(today.getMonth() - 100))
+  }
+
+  $("#certificate_date_issue, #certificate_initial_period, #certificate_final_period").singleDatePicker();
+
+
+
+});
