e99e395d5627873c6033f83187697a6eec7612f9
Adriano Avelino <adriano-st777@hotmail.com>
2021-02-07 14:51:01 -0300

    adiciona seed inicial para o banco de dados do sistema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "email@email.com", password: "123123")
+puts 'Cria usuário email@email.com'
+
+FactoryBot::create_list(:participant, 5)
+puts 'Cria 5 participantes'
+
+FactoryBot::create_list(:event, 5)
+puts 'Cria 5 eventos'
+
+FactoryBot::create_list(:emission_sector, 5)
+puts 'Cria 5 Setores de Emissão'
+
+FactoryBot::create_list(:certification_type, 5)
+puts 'Cria 5 Tipos de Certificados'
+
+FactoryBot::create_list(:certificate, 5)
+puts 'Cria 5 Certificados'
